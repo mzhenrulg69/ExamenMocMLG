9dc3eff30f07f3bf759032221a83f4891998ebcd
Maria Zhenru Lopez Gomez <mzhenrulg69@educastur.es>
2025-11-21 18:46:31 +0100

    añadir codigo que faltaba a controller

--- src/main/java/ExamenMocMLG/demo/Controller/ProductoController.java
+++ src/main/java/ExamenMocMLG/demo/Controller/ProductoController.java
@@ -37,7 +37,13 @@ public class ProductoController {
             - Si se indica el precio -> obtener los productos con ese precio.
             - Si se indica la categoria -> obtener los productos con esa categoria.
          */
-
+        if(precio==0 && categoria == null) {
+            return this.productoServiceImpl.findAllProductos();
+        }else if (precio != 0) {
+            return this.productoServiceImpl.findByPrecio(precio);
+        }else{
+            return this.productoServiceImpl.findByCategoria(categoria);
+        }
 
     }
 
